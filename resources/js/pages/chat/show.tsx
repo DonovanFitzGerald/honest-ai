20b70c82f43518313e89ee907300da7788fa6f1f
Donovan FitzGerald <Donovan.fitzg@gmail.com>
2026-03-10 18:55:15 +0000

    Prototype message display and input

--- resources/js/pages/chat/show.tsx
+++ resources/js/pages/chat/show.tsx
@@ -1,19 +1,51 @@
-import { Head } from '@inertiajs/react';
+import { useState } from 'react';
+import UserMessage from '@/components/ui/user-message';
 import AppLayout from '@/layouts/app-layout';
 
 export default function Show({ chat }) {
     const breadcrumbs = [
-        { title: 'Chat', href: '/chat' },
         { title: chat.title ?? `Chat #${chat.id}`, href: `/chat/${chat.id}` },
     ];
 
+    const [messages, setMessages] = useState([
+        'ipsum dolor sit amet consectetur adipisicing elit. Voluptates, quidem amet obcaecati odio reprehenderit enim ipsam natus temporibus iure maiores vero vitae consequatur harum saepe repellendus minima molestias autem excepturi',
+        'ipsum dolor sit amet consectetur adipisicing elit. Voluptates, quidem amet obcaecati odio reprehenderit enim ipsam natus temporibus iure maiores vero vitae consequatur harum saepe repellendus minima molestias autem excepturi',
+    ]);
+
+    const [inputText, setinputText] = useState('');
+    const handleInputChange = (e) => {
+        setinputText(e.target.value);
+    };
+
+    const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.key !== 'Enter') return;
+        e.preventDefault();
+        handleMessageSubmit(inputText);
+    };
+
+    const handleMessageSubmit = (text) => {
+        setMessages((prev) => {
+            return [...prev, text];
+        });
+    };
+
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
-            <Head title={chat.title ?? 'Chat'} />
-            <div className="p-4">
-                <h1 className="text-xl font-semibold">
-                    {chat.title ?? `Chat #${chat.id}`}
-                </h1>
+            <div className="flex flex-col gap-4 p-4">
+                {messages.map((m) => {
+                    return <UserMessage content={m} />;
+                })}
+            </div>
+            <div className="mt-auto flex w-full justify-center p-8">
+                <input
+                    type="text"
+                    className="w-full max-w-lg rounded-3xl border px-6 py-3"
+                    id="message-input"
+                    placeholder="Ask anything..."
+                    onChange={(e) => handleInputChange(e)}
+                    onKeyDown={handleKeyDown}
+                    value={inputText}
+                />
             </div>
         </AppLayout>
     );
